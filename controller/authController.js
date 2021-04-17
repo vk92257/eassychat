aa478c681b77fabb426c8e2d95a2b040aa665e2b
Vivek <vk92257@gmail.com>
2021-04-17 12:39:39 +0530

    added a protect route

---
+++ controller/authController.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken');
+const {promisify} = require("util");
+
+exports.protect = async (req,res,next)=>{
+    try {
+        if(!req.headers.authorization || !req.headers.authorization.startsWith("Bearer"))
+        {
+          return  res.status(400).json({
+                status:'fail',
+                message:"Please sign In",
+            });
+        }
+
+        const token = req.headers.authorization.split(" ")[1];
+     const decode = await   promisify(jwt.verify)(token,process.env.SECERTE_KEY);
+        console.log(decode);
+    //    / console.log(token);
+        next();
+    } catch (error) {
+        console.log(error);
+    }
+
+    
+}
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,8 @@
 const userController = require("./../controller/userContoller");
+const authController = require("./../controller/authController");
 const express = require("express");
 const router = express.Router();
 router.route("/sign-up").post(userController.signUp);
 router.route("/login").post(userController.login);
-router.route("/all-users").get(userController.allUser);
+router.route("/all-users").get(authController.protect,userController.allUser);
 module.exports = router;
